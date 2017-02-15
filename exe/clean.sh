701e2adf925c31698677513ceb090a1b2ab37b6a
Richard Roberts <rykardo.r@gmail.com>
2017-02-15 20:11:48 +1300

    Removed log files (etc) and added file extensions to cleanup script

--- exe/clean.sh
+++ exe/clean.sh
@@ -5,3 +5,27 @@ rm *.aux
 rm *.bbl
 rm *.blg
 rm *.sta
+rm *.fls
+rm *.fdb_latexmk
+
+
+rm */*.log
+rm */*.out
+rm */*.pdf
+rm */*.aux
+rm */*.bbl
+rm */*.blg
+rm */*.sta
+rm */*.fls
+rm */*.fdb_latexmk
+
+
+rm */*/*.log
+rm */*/*.out
+rm */*/*.pdf
+rm */*/*.aux
+rm */*/*.bbl
+rm */*/*.blg
+rm */*/*.sta
+rm */*/*.fls
+rm */*/*.fdb_latexmk
